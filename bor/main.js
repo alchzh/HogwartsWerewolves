aa685897f01bf705e10d74616407160a9ec44767
acz13 <alchzh@gmail.com>
2015-11-22 13:10:29 -0500

    do some formatting

--- bor/main.js
+++ bor/main.js
@@ -1,4 +1,13 @@
-
-function scrollDown() {
-    scrollBy(0, window.innerHeight);
-}
+$(document).ready(function() {
+    $('html body').scrollTop($('.first').offset().top);
+    $('#down').click( function() {
+        $('html,body').animate({
+           scrollTop: $(".second").offset().top
+        },500);
+    });
+    $('#up').click( function() {
+        $('html,body').animate({
+           scrollTop: $(".first").offset().top
+        },500);
+    });
+});

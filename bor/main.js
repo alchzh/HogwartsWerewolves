cf2ff37e9c73a00416a6c06a2643515bbb7c858c
acz13 <alchzh@gmail.com>
2015-11-22 19:56:26 -0500

    fix things

--- bor/main.js
+++ bor/main.js
@@ -1,25 +1,26 @@
-var page = 1;
+/*global $ */
+var currentPage = 1;
 
+function moveToPage(page) {
+    "use strict";
+    if (page < $('ul li').length) {
+        $('html,body').animate({
+            scrollTop: $('ul li:nth-child(' + page.toString() + ')').offset().top
+        }, 500);
+    }
+}
 
-$(document).ready(function() {
-    $('#down').click(moveDown);
-    $('#up').click(moveUp);
+$(document).ready(function () {
+    $('#down').click(function () {
+        if (currentPage < $('ul li').length) {
+            currentPage += 1;
+            moveToPage(currentPage);
+        }
+    });
+    $('#up').click(function () {
+        if (currentPage > 1) {
+            currentPage -= 1;
+            moveToPage(currentPage);
+        }
+    });
 });
-
-function moveDown() {
-    if (page < $('ul li').length){
-        page += 1;
-        $('html,body').animate({
-           scrollTop: $('ul li:nth-child(' + page.toString() + ')').offset().top
-        },500);
-    };
-};
-
-function moveUp() {
-    if (page > 1){
-        page -= 1;
-        $('html,body').animate({
-           scrollTop: $('ul li:nth-child(' + page.toString() + ')').offset().top
-        },500);
-    }; 
-};

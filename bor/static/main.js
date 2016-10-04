49f73970ad5c8013df86368733ce790198d8841f
Jay Fault <alchzh@gmail.com>
2016-10-04 14:36:10 +0000

    All the things

--- bor/static/main.js
+++ bor/static/main.js
@@ -1,22 +1,27 @@
 /*global $ */
-var currentPage = 1;
+"use scrict";
 
-function moveToPage(page) {
-    "use strict";
+function moveToPage(page, instant) {
     if (page < $('ul li').length + 1) {
         $('html,body').animate({
             scrollTop: $('ul li:nth-child(' + page.toString() + ')').offset().top
-        }, 500);
+        }, instant ? 0 : 500, "swing", function () {
+            window.location.hash = page.toString();
+        });
     }
 }
 
 $(document).ready(function () {
+    var hash = parseInt(window.location.hash.substring(1));
+    var currentPage = hash > 0 && hash < 11 ? hash : 1;
+    moveToPage(currentPage, true);
     $('#down').click(function () {
         if (currentPage < $('ul li').length) {
             currentPage += 1;
             moveToPage(currentPage);
         }
     });
+    
     $('#up').click(function () {
         if (currentPage > 1) {
             currentPage -= 1;

3fbd64dc3f71c6685a8f1449b8b56924d117fec2
Jay Fault <alchzh@gmail.com>
2017-01-10 17:57:26 +0000

    stuff

--- bor/static/main.js
+++ bor/static/main.js
@@ -1,6 +1,8 @@
 /*global $ */
 "use scrict";
 
+var scroll = true;
+
 function update(page) {
     if (page === 1) {
         $('#up').hide();
@@ -15,7 +17,7 @@ function update(page) {
 }
 
 function moveToPage(page, instant) {
-    if (page < $('ul li').length + 1) {
+    if (page <= $('ul li').length) {
         $('html,body').animate({
             scrollTop: $('ul li:nth-child(' + page.toString() + ')').offset().top
         }, instant ? 1 : 500, "swing", function () {
@@ -25,20 +27,29 @@ function moveToPage(page, instant) {
     update(page);
 }
 
+
 $(document).ready(function () {
     var hash = parseInt(window.location.hash.substring(1));
     var currentPage = hash > 0 && hash < 11 ? hash : 1;
-    moveToPage(currentPage, true);
-    $('#down').click(function () {
+    function up () {
+        if (currentPage > 1) {
+            currentPage -= 1;
+            moveToPage(currentPage);
+        }
+    }
+    function down() {
         if (currentPage < $('ul li').length) {
             currentPage += 1;
             moveToPage(currentPage);
         }
-    });
-    $('#up').click(function () {
-        if (currentPage > 1) {
-            currentPage -= 1;
-            moveToPage(currentPage);
+    }
+    moveToPage(currentPage, true);
+    $('#down').click(down);
+    $('#up').click(up);
+    $(document).keydown(function(e){
+        switch (e.which) {
+            case 38: up();
+            case 40: down();
         }
     });
 });
